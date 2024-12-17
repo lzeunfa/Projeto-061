57d3448b5b62f0fb425a964906f02e1f72f04da5
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-12-17 11:40:06 -0300

    progresso até a parte da logo

--- zero61.js
+++ zero61.js
@@ -2,6 +2,8 @@ var burger = document.getElementById(`burger`)
 
 var menu = document.querySelector(`div.menu`)
 
+var main = document.querySelector(`main`)
+
 function tamanho(){//para o burger ficar visivel em telas pequenas
     if (window.innerWidth <= 679){
         burger.style.display = 'block'
@@ -14,8 +16,10 @@ function clicar(){
     if(menu.style.display===`none`){
         menu.style.display=`block`
         burger.innerHTML=`close`
+        main.style.display=`none`
     }else{
         menu.style.display=`none`
         burger.innerHTML=`menu`
+        main.style.display=`block`
     }
 }
