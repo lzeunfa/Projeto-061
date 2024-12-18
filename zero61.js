d1210701043e6b51d2ed6b65c8836c55a9aaf024
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2024-12-18 12:32:41 -0300

    adicionei no JS function para ao clicar nas opcões do menu, o site role e mostre aonde elas estão

--- zero61.js
+++ zero61.js
@@ -1,10 +1,13 @@
-var burger = document.getElementById(`burger`)
+const burger = document.getElementById(`burger`)
 
-var menu = document.querySelector(`div.menu`)
+const menu = document.querySelector(`div.menu`)
 menu.style.display=`none`
 
-var main = document.querySelector(`main`)
+const main = document.querySelector(`main`)
 main.style.display=`block`
+
+const footer = document.querySelector(`footer`)
+footer.style.display=`block`
 
 function tamanho(){//para o burger ficar visivel em telas pequenas
     if (window.innerWidth <= 679){
@@ -19,9 +22,20 @@ function clicar(){
         menu.style.display=`block`
         burger.innerHTML=`close`
         main.style.display=`none`
+        footer.style.display=`none`
     }else{
         menu.style.display=`none`
         burger.innerHTML=`menu`
         main.style.display=`block`
+        footer.style.display=`block`
+    }
+}
+
+function clickDados(){
+    if(main.style.display===`none` && footer.style.display===`none`){
+        footer.style.display=`block`
+        main.style.display=`block`
+        menu.style.display=`none`
+        burger.innerHTML=`menu`
     }
 }
