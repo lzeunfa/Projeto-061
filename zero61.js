55abe76330cdff62bd2bb4526bac6fd60695e768
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2025-05-06 13:05:30 -0300

    dei uma organizada na parte de js, e mais alterações na area da sinfos nas boxdados alterei imagem dos icon s e suas cores

--- zero61.js
+++ zero61.js
@@ -1,42 +1,42 @@
-const burger = document.getElementById(`burger`)
+const burger = document.getElementById(`burger`);
 
-const menu = document.querySelector(`div.menuBurger`)
-menu.style.display=`none`
+const menu = document.querySelector(`div.menuBurger`);
+menu.style.display=`none`;
 
-const main = document.querySelector(`main`)
-main.style.display=`block`
+const main = document.querySelector(`main`);
+main.style.display=`block`;
 
-const footer = document.querySelector(`footer`)
-footer.style.display=`block`
+const footer = document.querySelector(`footer`);
+footer.style.display=`block`;
 
 function tamanho(){//para o burger ficar visivel em telas pequenas
     if (window.innerWidth <= 1023){
-        burger.style.display = 'block'
+        burger.style.display = 'block';
     } else {
-        burger.style.display = 'none'
+        burger.style.display = 'none';
     }
 }
 
 function clickMenu(){//para alterações do icon e dos elementos visiveis
     if(menu.style.display===`none` && main.style.display===`block`){
-        menu.style.display=`block`
-        burger.innerHTML=`close`
-        main.style.display=`none`
-        footer.style.display=`none`
+        menu.style.display=`block`;
+        burger.innerHTML=`close`;
+        main.style.display=`none`;
+        footer.style.display=`none`;
     }else{
-        menu.style.display=`none`
-        burger.innerHTML=`menu`
-        main.style.display=`block`
-        footer.style.display=`block`
+        menu.style.display=`none`;
+        burger.innerHTML=`menu`;
+        main.style.display=`block`;
+        footer.style.display=`block`;
     }
 }
 
 function clickDados(){//para os elementos ficarem visiveis ao serem buscados
     if(main.style.display===`none` && footer.style.display===`none`){
-        footer.style.display=`block`
-        main.style.display=`block`
-        menu.style.display=`none`
-        burger.innerHTML=`menu`
+        footer.style.display=`block`;
+        main.style.display=`block`;
+        menu.style.display=`none`;
+        burger.innerHTML=`menu`;
     }
 }
 
